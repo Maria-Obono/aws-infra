e33100a4d07fafa8fe81b4993654ba2958314ada
obonoondo <113368833+obonoondo@users.noreply.github.com>
2023-04-10 00:46:31 -0500

    Ass6 (#10)
    * assignment8
    
    * ass8

--- terraform-aws/module/networking/autoscaling-group.tf
+++ terraform-aws/module/networking/autoscaling-group.tf
@@ -7,8 +7,7 @@ resource "aws_autoscaling_group" "web_app_asg" {
   launch_template {
     id      = aws_launch_template.app_launch_template.id
     version = "$Latest"
-   }
-
+    }
 
   target_group_arns = [aws_lb_target_group.target_group.arn]
   
@@ -16,7 +15,9 @@ resource "aws_autoscaling_group" "web_app_asg" {
   min_size          = 1
   max_size          = 3
   desired_capacity  = 1
-  //health_check_grace_period = 60
+
+  health_check_grace_period = 300
+
   health_check_type     = "EC2"
 
   //defining the termination policy where the instance with the least connections will be terminated first
@@ -55,12 +56,5 @@ output "autoscaling_group_id" {
 value = aws_autoscaling_group.web_app_asg.id
 }
 
-  
 
-
-// resource "aws_network_interface_sg_attachment" "network_interfaces" {
-  
- // security_group_id    = aws_security_group.app_sg.id
-  //network_interface_id = aws_network_interface.eni.id
-//}
  
