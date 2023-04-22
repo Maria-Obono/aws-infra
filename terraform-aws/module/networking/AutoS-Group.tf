5360df69d72947dd49f8239313d15810509044d0
Maria Gloria Raquel Obono <obonoondo.m@northeastern.edu>
2023-04-22 07:11:58 -0400

    fixed

--- terraform-aws/module/networking/AutoS-Group.tf
+++ terraform-aws/module/networking/AutoS-Group.tf
@@ -1,11 +1,11 @@
 //Define autoscaling group
 resource "aws_autoscaling_group" "web_app_asg" {
   name         = "asg_launch_config"
-  vpc_zone_identifier = [aws_subnet.public-subnet[0].id, aws_subnet.public-subnet[1].id]
-  
+  vpc_zone_identifier = [aws_subnet.public-subnet[0].id, aws_subnet.public-subnet[1].id, aws_subnet.public-subnet[2].id]
+  //vpc_id      = aws_vpc.maria.id
   launch_template {
     id      = aws_launch_template.app_launch_template.id
-    version = "$Latest"
+    version = aws_launch_template.app_launch_template.latest_version
     }
 
   target_group_arns = [aws_lb_target_group.target_group.arn]
@@ -14,8 +14,8 @@ resource "aws_autoscaling_group" "web_app_asg" {
   min_size          = 1
   max_size          = 3
   desired_capacity  = 1
-  health_check_grace_period = 90
-  health_check_type     = "ELB"
+  health_check_grace_period = 600
+  health_check_type     = "EC2"
   termination_policies      = ["Default"]
 //instance_refresh {
     //strategy = "Rolling"
@@ -27,22 +27,42 @@ resource "aws_autoscaling_group" "web_app_asg" {
       propagate_at_launch = true
     }
      tag {
-    key                 = "Environment"
-    value               = "Development"
+    key                 = "Application"
+    value               = "webApp"
     propagate_at_launch = true
   }
+# instance scale-in protection configuration
+  protect_from_scale_in = true
   
 lifecycle {
-create_before_destroy = true
+ignore_changes = [
+      //"tag",
+      //"desired_capacity",
+      desired_capacity,
+      min_size,
+      max_size,
+      launch_template,
+      target_group_arns,
+      health_check_type,
+      health_check_grace_period,
+      vpc_zone_identifier,
+      termination_policies,
+    ]
+    create_before_destroy = true
 }
-//metric {
-    //name = "CPUUtilization"
-    //namespace = "AWS/EC2"
-    //statistic = "Average"
-    //period = 60
-    //unit = "Percent"
-  //}
 
+ 
+
+}
+
+resource "aws_autoscaling_lifecycle_hook" "example" {
+  name                 = "example-lifecycle-hook"
+  autoscaling_group_name = aws_autoscaling_group.web_app_asg.name
+  default_result       = "CONTINUE"
+  heartbeat_timeout    = 300
+  lifecycle_transition = "autoscaling:EC2_INSTANCE_TERMINATING"
+
+  
 }
 
 
@@ -70,6 +90,38 @@ resource "aws_iam_role_policy_attachment" "asg_policy_attachment" {
 }
 
 resource "aws_autoscaling_attachment" "asg_attachment_bar" {
-  autoscaling_group_name = aws_autoscaling_group.web_app_asg.id
+  autoscaling_group_name = aws_autoscaling_group.web_app_asg.name
   alb_target_group_arn   = aws_lb_target_group.target_group.arn
 }
+
+resource "aws_lb_listener_rule" "web_app_listener_rule" {
+  listener_arn = aws_lb_listener.alb_https_listener.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+
+  }
+
+  condition {
+    path_pattern {
+      values = ["/health"]
+    }
+  }
+  condition {
+    host_header {
+      values = ["${aws_lb.load_balancer.dns_name}"]
+      
+    }
+  }
+
+  condition {
+    http_request_method {
+      values = ["GET"]
+    }
+  }
+
+ 
+}
+
--- terraform-aws/module/networking/Launch_template.tf
+++ terraform-aws/module/networking/Launch_template.tf
@@ -1,62 +1,55 @@
+
+
 resource "aws_launch_template" "app_launch_template" {
-   name        = "asg_launch_config"
-  image_id = data.aws_ami.app_ami.id
-  instance_type = var.settings.web_app.instance_type
-  key_name = "Key"
-  
-  placement {
-    availability_zone = data.aws_availability_zones.available.id
-  }
-
-    network_interfaces {
-    device_index = 0
-    associate_public_ip_address = true
-    subnet_id       = aws_subnet.public-subnet[0].id
-    security_groups = [aws_security_group.app_sg.id]
-    
-  }
-  iam_instance_profile {
-   arn= aws_iam_instance_profile.maria_profile.arn
-  }
-
-  user_data = base64encode(data.template_file.user_data.rendered)
-
-  disable_api_termination = false // Set this to false to disable protection against accidental termination
-  monitoring {
-    enabled = true
-  }
-  metadata_options {
-    http_endpoint = "enabled"
-    http_tokens = "required"
-    http_put_response_hop_limit = 1
-    instance_metadata_tags = "enabled"
-  }
+  name_prefix             = "web_application"
+  image_id                = data.aws_ami.app_ami.id
+  instance_type           = var.settings.web_app.instance_type
+  key_name                = "Key"
  
+  disable_api_termination = false
 
   block_device_mappings {
-  device_name = "/dev/sda1"
-  ebs {
-    volume_size = 50
-    volume_type = "gp2"
-    encrypted = true
-    delete_on_termination = true
-    kms_key_id = aws_kms_key.customer_managed_key.arn
-  }
-}
-    tag_specifications {
-    resource_type = "instance"
-    tags = {
-      Name        = "asg_launch_config"
-      vpc_id      = aws_vpc.maria.id
-      role        = aws_iam_role.EC2-CSYE6225.name
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size = 50
+      volume_type = "gp2"
+      encrypted   = true
+      kms_key_id  = aws_kms_key.customer_managed_key.arn
     }
   }
 
-lifecycle {
+  user_data = base64encode(data.template_file.user_data.rendered)
+monitoring {
+    enabled = true
+  }
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_put_response_hop_limit = 2
+    http_tokens                 = "optional"
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      "Name"  = "Terraform EC2"
+      "vpc_id" = aws_vpc.maria.id
+      "role"   = aws_iam_role.EC2-CSYE6225.name
+    }
+  }
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.app_sg.id]
+    subnet_id                   = "${aws_subnet.public-subnet[0].id}"
+  }
+
+  hibernation_options {
+    configured = false
+  }
+  lifecycle {
   create_before_destroy = true
 }
 
- 
+
 }
 
 resource "aws_kms_key" "customer_managed_key" {
